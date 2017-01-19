10c36f938d4e7339a219c155fed5ffc0e5a3de91
Kruchy <karol@krusiec.com>
2017-01-19 13:55:28 +0100

    preparing the server environment - stage 1

---
+++ layout/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="BabyApp" />
+  <meta name="keywords" content="happiness, useful, easy, parenting, baby, clear" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php if(isset($title)){ echo $title; }?></title>
+    <!--<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">-->
+    <!--<link rel="stylesheet" href="style/main.css">-->
+    <link rel="stylesheet" href="style/style.css" />
+</head>
+<body>
---
+++ layout/reglogheader.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="BabyApp" />
+  <meta name="keywords" content="happiness, useful, easy, parenting, baby, clear" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php if(isset($title)){ echo $title; }?></title>
+    <link rel="stylesheet" href="style/reglog.css" />
+</head>
+<body>
