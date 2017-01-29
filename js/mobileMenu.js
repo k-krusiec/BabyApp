a56f6c826801ac03ed1671b149c8b270ab23a5e6
Kruchy <karol@krusiec.com>
2017-01-29 10:44:35 +0100

    mobile menu js file - update

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,29 +1,31 @@
-//mobile-menu
-function mobileMenuHendling() {
-  var $hamMenuBtn = $('.ham-menu');
-  var $mobileMenu = $('.mobile-menu');
-  if (matchMedia) {
-    var mqTab = window.matchMedia("(min-width: 960px)");
-    mqTab.addListener(WidthChange);
-    WidthChange(mqTab);
+$(document).ready(function() {
+
+  function mobileMenuHendling() {
+    var $hamMenuBtn = $('.ham-menu');
+    var $mobileMenu = $('.mobile-menu');
+    if (matchMedia) {
+      var mqTab = window.matchMedia("(min-width: 960px)");
+      mqTab.addListener(WidthChange);
+      WidthChange(mqTab);
+    }
+
+    function WidthChange(mqTab) {
+      if (mqTab.matches) {
+        $mobileMenu.css({'display': 'none'});
+        $hamMenuBtn.removeClass('ham-close-icon').addClass('ham-icon');
+      }
+    }
+
+    $hamMenuBtn.on('click', function() {
+      if ($mobileMenu.css('display') !== 'none') {
+        $mobileMenu.css({'display': 'none'});
+        $(this).removeClass('ham-close-icon').addClass('ham-icon');
+      } else {
+        $mobileMenu.css({'display': 'block'});
+        $(this).removeClass('ham-icon').addClass('ham-close-icon');
+      }
+    })
   }
 
-  function WidthChange(mqTab) {
-    if (mqTab.matches) {
-      $mobileMenu.css({'display': 'none'});
-      $hamMenuBtn.removeClass('ham-close-icon').addClass('ham-icon');
-    }
-  }
-
-  $hamMenuBtn.on('click', function() {
-    if ($mobileMenu.css('display') !== 'none') {
-      $mobileMenu.css({'display': 'none'});
-      $(this).removeClass('ham-close-icon').addClass('ham-icon');
-    } else {
-      $mobileMenu.css({'display': 'block'});
-      $(this).removeClass('ham-icon').addClass('ham-close-icon');
-    }
-  })
+  mobileMenuHendling();
 }
-mobileMenuHendling();
-//mobile-menu end
